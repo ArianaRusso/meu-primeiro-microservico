3d40aa8182596feb2dded5bca85f736e5441adb2
Russo <anuo@gft.com>
2022-12-13 15:16:33 -0300

    login, jwt

---
+++ hr-oauth/src/main/java/com/gft/hruser/hroauth/config/SecurityConfig.java
@@ -0,0 +1,36 @@
+package com.gft.hruser.hroauth.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	 
+	@Autowired
+	private BCryptPasswordEncoder passwordEnconder;
+	
+	@Autowired
+	private UserDetailsService userDetailService;
+
+	@Override
+	@Autowired
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailService).passwordEncoder(passwordEnconder);
+	}
+
+	@Override
+	@Bean 
+	protected AuthenticationManager authenticationManager() throws Exception {
+		return super.authenticationManager();
+	}
+	
+	
+	
+	
+}
